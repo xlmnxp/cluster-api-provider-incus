6b0e690c1ec13b9765c0e0b5f76a09095139e417
Angelos Kolaitis <neoaggelos@gmail.com>
2025-12-18 14:58:50 +0100

    update containernetworking/plugins v1.8.0 -> v1.9.0 (#179)
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- internal/static/embed/install-kubeadm.sh
+++ internal/static/embed/install-kubeadm.sh
@@ -6,7 +6,7 @@
 set -xeu
 
 KUBERNETES_VERSION="${KUBERNETES_VERSION:-$1}"            # https://dl.k8s.io/release/stable.txt or https://dl.k8s.io/release/stable-1.32.txt
-CNI_PLUGINS_VERSION="${CNI_PLUGINS_VERSION:-v1.8.0}"      # https://github.com/containernetworking/plugins
+CNI_PLUGINS_VERSION="${CNI_PLUGINS_VERSION:-v1.9.0}"      # https://github.com/containernetworking/plugins
 CRICTL_VERSION="${CRICTL_VERSION:-v1.34.0}"               # https://github.com/kubernetes-sigs/cri-tools
 CONTAINERD_VERSION="${CONTAINERD_VERSION:-v2.2.0}"        # https://github.com/containerd/containerd
 RUNC_VERSION="${RUNC_VERSION:-v1.3.3}"                    # https://github.com/opencontainers/runc, must match https://raw.githubusercontent.com/containerd/containerd/${CONTAINERD_VERSION}/script/setup/runc-version
